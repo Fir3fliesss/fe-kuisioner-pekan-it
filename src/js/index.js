763a73b5c5105680abda05c23f5bb747f883e0b0
adityakurnias <adityakurniasaputra903@gmail.com>
2024-12-17 00:39:29 +0700

    Sudah selesai bang, sisa sambungin ke be

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,19 @@
 const loading = document.getElementById("loading");
+const bukaList = document.getElementById("BukaList");
+const kelas = document.getElementById("kelas");
+const main = document.getElementById("main");
 
 window.addEventListener("load", () => {
-    loading.style.display = "none"
-})
+  loading.classList.remove("flex");
+  loading.classList.add("hidden");
+  main.classList.remove("hidden");
+  main.classList.add("block");
+});
+
+bukaList.addEventListener("click", () => {
+  if (kelas.classList.contains("max-h-0")) {
+    kelas.classList.replace("max-h-0", "max-h-[2000px]");
+  } else {
+    kelas.classList.replace("max-h-[2000px]", "max-h-0");
+  }
+});
